5305a5359601b0cbaa1cff850dad8a1b8d0aa727
kermage <genealyson.torcende@gmail.com>
2020-02-08 06:59:16 +0800

    do similar method from core

--- Preload.php
+++ Preload.php
@@ -10,4 +10,25 @@
 namespace ThemePlate;
 
 class Preload {
+
+	public static function init() {
+
+		global $wp_scripts, $wp_styles;
+
+		foreach ( array( $wp_scripts, $wp_styles ) as $dependencies ) {
+			if ( ! empty( $dependencies->queue ) ) {
+				foreach( $dependencies->queue as $handle ) {
+					if ( ! isset( $dependencies->registered[ $handle ] ) ) {
+						continue;
+					}
+
+					$dependency = $dependencies->registered[ $handle ];
+
+					echo "<link rel='preload' href='{$dependency->src}' />\n";
+				}
+			}
+		}
+
+	}
+
 }
